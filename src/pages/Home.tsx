33ed17749c640b254e4c3fa8603e3e057803c6d3
Habeeb Moosa <habeebmoosadev@gmail.com>
2024-01-22 01:14:47 +0530

    Refactor Home component UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,19 @@ import { useNavigate } from "react-router-dom";
 
 export const Home: React.FC = () => {
     const[username, setUsername] = useState<string>("")
-    const naviage = useNavigate();
+    const navigate = useNavigate();
 
     const sendUsername = ()=> {
-        naviage("/dashboard", {state: {username: username}})
+        navigate("/dashboard", {state: {username: username}})
     }
 
     return (
-        <div className="flex flex-col item-center">
-            <h1>Home</h1>
-            <div className="flex flex-row -2">
-                <input type="text" placeholder="Enter Username" onChange={(e)=>{setUsername(e.target.value)}}></input>
-                <button type="button" onClick={sendUsername}>Enter</button>
-            </div> 
+        <div className="flex flex-col">
+            <h1 className="text-3xl">Github Profiler</h1>
+            <div className="flex flex-row gap-2 p-10 bg-gray-300 rounded-md mt-10">
+                <input type="text" className="p-2 rounded-md" placeholder="Enter Username" onChange={(e)=>{setUsername(e.target.value)}}></input>
+                <button className="p-2 bg-slate-600 px-4 rounded-md text-white" type="button" onClick={sendUsername}>Enter</button>
+            </div>  
         </div>
     )
 }
