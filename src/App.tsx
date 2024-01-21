08e7329845ca3ec988ab2c203ca4c17b34bcf810
Habeeb Moosa <habeebmoosadev@gmail.com>
2024-01-22 00:08:44 +0530

    Add Dashboard page to App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter} from 'react-router-dom';
 import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Contact } from './pages/Contact';
+import { Dashboard } from './pages/Dashboard';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <div className='app'>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/dashboard' element={<Home />} />
+          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
