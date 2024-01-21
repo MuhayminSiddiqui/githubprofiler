85bb3411bb8afbec00a0ee7376c5b8b10c019a81
Habeeb Moosa <habeebmoosadev@gmail.com>
2024-01-22 01:08:59 +0530

    Add username state and display it in Dashboard component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export const Dashboard: React.FC = () => {
+    const [username, setUsername] = useState<string>("");
+
+    const location = useLocation();
+
+    useEffect(()=>{
+        if(location.state && location.state.username){
+            setUsername(location.state.username)
+        }
+    }, [location.state, username])
+
     return (
         <div>
             <h1>Dashboard</h1>
+            <p>{username}</p>
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,20 @@
 import React,{useState} from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Home: React.FC = () => {
     const[username, setUsername] = useState<string>("")
+    const naviage = useNavigate();
+
     const sendUsername = ()=> {
-        alert("Username:" + username)
+        naviage("/dashboard", {state: {username: username}})
     }
+
     return (
         <div className="flex flex-col item-center">
             <h1>Home</h1>
             <div className="flex flex-row -2">
                 <input type="text" placeholder="Enter Username" onChange={(e)=>{setUsername(e.target.value)}}></input>
-                <p>{username}</p>
-                <button onClick={sendUsername}>Enter</button>
+                <button type="button" onClick={sendUsername}>Enter</button>
             </div> 
         </div>
     )
